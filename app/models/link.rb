58d54fdc788f19be8f467e20093728052fe7264e
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2016-12-06 19:54:51 -0300

    New methods for generate out_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,12 @@
 class Link < ApplicationRecord
+  after_create :generate_slug
+
+  def generate_slug
+    self.slug = self.id.hash.abs.to_s(36)[0,6]
+    self.save
+  end
+
+  def out_url
+    ENV['BASE_URL'] + self.slug
+  end
 end
