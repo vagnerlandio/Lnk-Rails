a771fd01823f848f4412655961e3b9cbcef4de52
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2016-12-06 20:04:08 -0300

    Config new controller link

---
+++ app/controllers/link_controller.rb
@@ -0,0 +1,37 @@
+class LinkController < ApplicationController
+  def show
+    if params[:slug]
+      @link = Link.find_by(slug: params[:slug])
+      if redirect_to @link.in_url
+        @link.clicks += 1
+        @link.save
+      end
+    else
+      @link = Link.find(params[:id])
+    end
+  end
+
+  def create
+    @link = Link.new(link_params)
+
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to root_path, notice: 'Link was successfully created.' }
+        format.js
+        format.json { render action: 'show', status: :created, location: @link }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_link
+      @link = Link.find_by(slug: params[:slug])
+    end
+
+    def link_params
+      params.require(:link).permit(:in_url)
+    end
+end
